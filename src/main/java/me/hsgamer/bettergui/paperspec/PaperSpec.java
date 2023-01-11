4a77979428b7ea10347edd711d2dee03a006ea90
Huynh Tien <huynhqtienvtag@gmail.com>
2023-01-11 18:30:11 +0700

    Revert "Add support to use MiniMessage format for inventory title (#3)" (#4)
    This reverts commit b9d23ee1c5a9e4069ff8e3d45e6161496196af95.

--- src/main/java/me/hsgamer/bettergui/paperspec/PaperSpec.java
+++ src/main/java/me/hsgamer/bettergui/paperspec/PaperSpec.java
@@ -1,20 +1,12 @@
 package me.hsgamer.bettergui.paperspec;
 
-import me.hsgamer.bettergui.builder.InventoryBuilder;
 import me.hsgamer.bettergui.builder.ItemModifierBuilder;
 import me.hsgamer.bettergui.paperspec.modifier.AdventureLoreModifier;
 import me.hsgamer.bettergui.paperspec.modifier.AdventureNameModifier;
 import me.hsgamer.bettergui.paperspec.modifier.PaperSkullModifier;
-import me.hsgamer.bettergui.paperspec.util.AdventureUtils;
 import me.hsgamer.hscore.bukkit.addon.PluginAddon;
-import me.hsgamer.hscore.bukkit.gui.GUIHolder;
-import me.hsgamer.hscore.bukkit.gui.GUIUtils;
 import me.hsgamer.hscore.common.Validate;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.InventoryHolder;
 
 import java.util.List;
 
@@ -23,38 +15,21 @@ public final class PaperSpec extends PluginAddon {
     public boolean onLoad() {
         return (Validate.isClassLoaded("com.destroystokyo.paper.PaperConfig") ||
                 Validate.isClassLoaded("io.papermc.paper.configuration.Configuration")
-               ) &&
-               (Validate.isClassLoaded("net.kyori.adventure.text.Component") &&
-                Validate.isClassLoaded("net.kyori.adventure.text.minimessage.MiniMessage") &&
-                Validate.isClassLoaded("net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer") &&
-                Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "lore", List.class) &&
-                Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "lore") &&
-                Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "displayName", Component.class) &&
-                Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "displayName") &&
-                Validate.isMethodLoaded("org.bukkit.Bukkit", "createInventory", InventoryHolder.class, int.class, Component.class)
-               );
+        ) &&
+                (Validate.isClassLoaded("net.kyori.adventure.text.Component") &&
+                        Validate.isClassLoaded("net.kyori.adventure.text.minimessage.MiniMessage") &&
+                        Validate.isClassLoaded("net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer") &&
+                        Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "lore", List.class) &&
+                        Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "lore") &&
+                        Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "displayName", Component.class) &&
+                        Validate.isMethodLoaded("org.bukkit.inventory.meta.ItemMeta", "displayName")
+                );
     }
 
     @Override
     public void onEnable() {
-        ItemModifierBuilder.INSTANCE.register(PaperSkullModifier::new, "paper-skull", "paper-head", "skull$", "head$");
-        ItemModifierBuilder.INSTANCE.register(AdventureNameModifier::new, "mini-name", "name$");
-        ItemModifierBuilder.INSTANCE.register(AdventureLoreModifier::new, "mini-lore", "lore$");
-
-        InventoryBuilder.INSTANCE.register(pair -> (display, uuid) -> {
-            GUIHolder holder = display.getHolder();
-            InventoryType type = holder.getInventoryType();
-            int size = holder.getSize(uuid);
-
-            // MiniMessage deserializer will throw a runtime exception
-            // if the string contains any legacy color codes (&1, &b).
-            // So we strip it in case the menu fails to be loaded.
-            String title = ChatColor.stripColor(holder.getTitle(uuid));
-
-            Component adventure$title = AdventureUtils.toComponent(title);
-            return type == InventoryType.CHEST && size > 0
-                   ? Bukkit.createInventory(display, GUIUtils.normalizeToChestSize(size), adventure$title)
-                   : Bukkit.createInventory(display, type, adventure$title);
-        }, "mini-title");
+        ItemModifierBuilder.INSTANCE.register(PaperSkullModifier::new, "paper-skull", "paper-head");
+        ItemModifierBuilder.INSTANCE.register(AdventureNameModifier::new, "mini-name", "adventure-name", "name$");
+        ItemModifierBuilder.INSTANCE.register(AdventureLoreModifier::new, "mini-lore", "adventure-lore", "lore$");
     }
 }
