100ec7f2ba9ca28d1e3c72d69b05d0352fb1dae3
HSGamer <huynhqtienvtag@gmail.com>
2023-01-29 10:00:37 +0700

    update core

--- src/main/java/me/hsgamer/bettergui/paperspec/inventory/ComponentInventoryBuilder.java
+++ src/main/java/me/hsgamer/bettergui/paperspec/inventory/ComponentInventoryBuilder.java
@@ -1,9 +1,9 @@
 package me.hsgamer.bettergui.paperspec.inventory;
 
 import me.hsgamer.bettergui.paperspec.util.AdventureUtils;
-import me.hsgamer.hscore.bukkit.gui.GUIDisplay;
-import me.hsgamer.hscore.bukkit.gui.GUIHolder;
-import me.hsgamer.hscore.bukkit.gui.GUIUtils;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIDisplay;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIHolder;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIUtils;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -13,11 +13,11 @@ import org.bukkit.inventory.Inventory;
 import java.util.UUID;
 import java.util.function.BiFunction;
 
-public class ComponentInventoryBuilder implements BiFunction<GUIDisplay, UUID, Inventory> {
+public class ComponentInventoryBuilder implements BiFunction<BukkitGUIDisplay, UUID, Inventory> {
 
     @Override
-    public Inventory apply(GUIDisplay display, UUID uuid) {
-        GUIHolder holder = display.getHolder();
+    public Inventory apply(BukkitGUIDisplay display, UUID uuid) {
+        BukkitGUIHolder holder = display.getHolder();
         InventoryType type = holder.getInventoryType();
         int size = holder.getSize(uuid);
 
@@ -27,9 +27,9 @@ public class ComponentInventoryBuilder implements BiFunction<GUIDisplay, UUID, I
         // See: https://github.com/KyoriPowered/adventure/blob/529d407d55ca50e6dee10f5696063f97e34ae9b5/api/src/main/java/net/kyori/adventure/text/TextComponentImpl.java#L40
         String title = ChatColor.stripColor(holder.getTitle(uuid));
 
-        Component adventure$title = AdventureUtils.toComponent(title);
+        Component adventureTitle = AdventureUtils.toComponent(title);
         return type == InventoryType.CHEST && size > 0
-                ? Bukkit.createInventory(display, GUIUtils.normalizeToChestSize(size), adventure$title)
-                : Bukkit.createInventory(display, type, adventure$title);
+                ? Bukkit.createInventory(display, BukkitGUIUtils.normalizeToChestSize(size), adventureTitle)
+                : Bukkit.createInventory(display, type, adventureTitle);
     }
 }
